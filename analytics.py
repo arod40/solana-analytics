071417e7f6d8141e7b91ee68f140765d0d61b747
arod40 <arodacademic@gmail.com>
2022-04-30 18:06:17 -0500

    Analytics leader schedule pie chart

--- analytics.py
+++ analytics.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -5,6 +6,8 @@ import matplotlib.pyplot as plt
 from models import Block
 from utils.constants import *
 from utils.plot import plot_bars
+
+import json
 
 
 def plot_rent_collected(data_dir, slot_range):
@@ -42,6 +45,36 @@ def plot_number_of_transactions(data_dir, slot_range):
     plt.show()
 
 
+def plot_leaders_pie_chart(data_dir, epoch, other_share=1 / 3):
+    schedule = json.loads((data_dir / str(epoch) / "leader_schedule.json").read_text())
+    data = []
+    total = 0
+    for pubkey, slots in schedule.items():
+        n = len(slots)
+        data.append((n, pubkey))
+        total += n
+
+    data.sort(reverse=True)
+
+    labels = []
+    fracs = []
+    acc = 0
+    for num, pubkey in data:
+        if acc / total >= 1 - other_share:
+            labels.append("other")
+            fracs.append(total - acc)
+            break
+        labels.append(pubkey)
+        fracs.append(num)
+        acc += num
+
+    _, ax = plt.subplots(1, 1)
+    ax.pie(fracs, labels=labels)
+
+    plt.show()
+
+
 if __name__ == "__main__":
     plot_rent_collected(Path("data"), list(range(131856396, 131856404)))
     plot_number_of_transactions(Path("data"), list(range(131856396, 131856404)))
+    plot_leaders_pie_chart(Path("data"), 304)
