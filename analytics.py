00aefedac445c9de721db54528c1c5b51fa89577
arod40 <arodacademic@gmail.com>
2022-04-30 12:35:14 -0500

    Analytics: Transactions

--- analytics.py
+++ analytics.py
@@ -24,5 +24,24 @@ def plot_rent_collected(data_dir, slot_range):
     plt.show()
 
 
+def plot_number_of_transactions(data_dir, slot_range):
+    total = {}
+    fail = {}
+    for slot in slot_range:
+        block: Block = Block.from_json(data_dir / f"{slot}.json")
+        total[slot] = len(block.transactions)
+        fail[slot] = len([tr for tr in block.transactions if tr.err is not None])
+
+    _, ax = plt.subplots(1, 1)
+
+    plot_bars(ax, total, "green", "total", move=0, width=0.25)
+    plot_bars(ax, fail, "red", "fail", move=0, width=0.25)
+    ax.set_xlabel("slots")
+    ax.set_ylabel("number of transactions")
+    plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
     plot_rent_collected(Path("data"), list(range(131856396, 131856404)))
+    plot_number_of_transactions(Path("data"), list(range(131856396, 131856404)))
