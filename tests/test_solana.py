06a407048f5b454fabc6110bcce09fccc6fe66d5
arod40 <arodacademic@gmail.com>
2022-05-03 00:48:47 -0500

    Remove tests directory

--- tests/test_solana.py
+++
@@ -1,7 +0,0 @@
-from config import *
-from solana.rpc.api import Client
-
-
-def test_connectivity():
-    http_client = Client(f"https://api.{CLUSTER}.solana.com")
-    assert http_client.is_connected(), f"Can't connect to the cluster {CLUSTER}"
